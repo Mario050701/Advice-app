83a6cd1b80eab2d6ffa91a024c86b8cf7d1ebf30
Mario Martinovic <mariomartinovic2001@gmail.com>
2025-09-08 15:44:19 +0200

    Implement light-dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
 import Cube from "./components/Cube.jsx";
 import Quote from "./components/Quote";
+import { createContext, useState, useEffect } from "react";
+import { GiPowerButton } from "react-icons/gi";
+
+export const ThemeContext = createContext(null);
 
 import "./components/quote.css";
 import "./index.css";
 
 export default function App() {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    document.body.id = theme;
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((current) => (current === "light" ? "dark" : "light"));
+  };
+
   return (
-    <div>
-      <Quote />
-      <Cube />
-    </div>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <GiPowerButton
+        onClick={toggleTheme}
+        className={`light-dark-btn ${theme}`}
+      />
+
+      <div>
+        <Quote />
+        <Cube />
+      </div>
+    </ThemeContext.Provider>
   );
 }
